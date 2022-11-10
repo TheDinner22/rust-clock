7e24af9059bf7b4d666856c698da9abcb6f85ff8
joe goodman <jgood1133@gmail.com>
2022-11-10 18:34:44 -0500

    first commit

---
+++ logic/src/lib.rs
@@ -0,0 +1,14 @@
+struct ClockInformation {}
+
+impl ClockInformation {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
